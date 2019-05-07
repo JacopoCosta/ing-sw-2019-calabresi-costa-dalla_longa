75f37013bf0037a08983f4e9424b98ef23d06231
JacopoCosta <jacopo.costa@mail.polimi.it>
2019-05-07 16:20:22 +0200

    Updated lambda function in Board.

--- src/main/java/it/polimi/ingsw/model/board/Board.java
+++ src/main/java/it/polimi/ingsw/model/board/Board.java
@@ -9,7 +9,9 @@ import it.polimi.ingsw.model.powerups.PowerUp;
 import it.polimi.ingsw.model.weaponry.Weapon;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Board {
     private List<Player> killers;
@@ -53,25 +55,14 @@ public class Board {
     }
 
     public void sortCells(List<Cell> cells) {
-        int tempCoordinateX;
-        int tempCoordinateY;
-        for(int i=0; i<cells.size(); i++) {
-            tempCoordinateX = cells.get(0).getXCoord();
-            tempCoordinateY = cells.get(0).getYCoord();
-
-            if(tempCoordinateY > cells.get(i).getYCoord()) {
-                //the element is not in the right place
-                cells.add(0, cells.get(i));
-                cells.remove(i);
-                i=0; //starts over
-            }
-            else if(tempCoordinateX > cells.get(i).getXCoord()) {
-                //the element is not in the right place
-                cells.add(0, cells.get(i));
-                cells.remove(i);
-                i=0; //starts over
-            }
-        }
+        Comparator<Cell> before = (c1, c2) -> {
+            if(c1.getYCoord() == c2.getYCoord())
+                return c1.getYCoord() - c2.getYCoord();
+            return c1.getXCoord() - c2.getXCoord();
+        };
+        cells = cells.stream()
+                    .sorted(before)
+                    .collect(Collectors.toList());
     }
 
     public Cell findSpawnPoint(AmmoCubes ammoCubeColor) {
